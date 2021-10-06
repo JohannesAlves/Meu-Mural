168b772124bf22ac39a2d1efd50286a50fa07462
Johannes Alves <johannes001002@gmail.com>
2021-10-05 22:22:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const PORT = 3000;
+const express = require('express');
+const apiRoute = require('./routes/api');
+
+const path = require('path');
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, "public")));
+app.use('/api', apiRoute);
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server Running on:${PORT}`);
+})
+
